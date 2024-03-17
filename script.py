5d77c1594b108cf2eb16065d62f66ec7ed733c11
Jonathas Rodrigues <jonathashnr@gmail.com>
2024-03-17 19:12:03 -0300

    Isola escolha de progressão e cria main func

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 
-# Função de primeira alta com closure
+# Função de ALTA ORDEM com CLOSURE
 def validade_function(prompt, validate_type):
     err_message = 'Tipo de entrada inválido, a entrada deve ser do tipo: ' + str(validate_type)
     def validator():
@@ -14,20 +14,24 @@ def validade_function(prompt, validate_type):
     return validator
 
 # Outra função de alta ordem,
-# mas agora com função lambda e list comprehension
+# mas agora com FUNÇÃO LAMBDA e LIST COMPREHENSION
 def prog_function(tipo):
     if tipo == 'pa':
         return lambda r, e1, n: [e1 + i * r for i in range(n)]  # list comprehension e função lambda
     else:
         return lambda r, e1, n: [e1 * r ** i for i in range(n)] 
 
-while True:
+def whatprog():
     while True:
         prog = input('Você quer calcular uma progressão aritimética(PA) ou uma progressão geométrica(PG)? ').lower()
         if prog == 'pa' or prog == 'pg':
             break
         else:
             print('Entrada inválida, as únicas entradas válidas são PA e PG.')
+    return prog
+
+def main():
+    prog = whatprog()
     validate_razao = validade_function('Qual é a razão da progressão? ', float)
     validate_e1 = validade_function('Qual é o valor do primeiro elemento? ', float)
     validate_n = validade_function('Quantos elementos deve ter a progressão? ', int)
@@ -36,7 +40,7 @@ while True:
     n = validate_n()
     prog_func = prog_function(prog)
     prog_list = prog_func(razao,e1,n)
-    print(prog_list)
-    wanna_quit = input('Deseja calcular mais uma progressão? ').lower()
-    if wanna_quit != 's' and wanna_quit != 'sim':
-        break
+    print(str(prog_list))
+
+if __name__ == '__main__':
+    main()
